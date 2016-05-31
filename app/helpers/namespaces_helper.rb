8f30ee7dd20b6234c010d46afe0691546d6efd6b
Miquel Sabaté Solà <msabate@suse.com>
2016-05-31 15:00:55 +0200

    Only show the needed circles
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- app/helpers/namespaces_helper.rb
+++ app/helpers/namespaces_helper.rb
@@ -1,5 +1,21 @@
 module NamespacesHelper
   def can_manage_namespace?(namespace)
-    current_user.admin? || namespace.team.owners.exists?(current_user.id)
+    current_user.admin? || owner?(namespace)
+  end
+
+  def owner?(namespace)
+    namespace.team.owners.exists?(current_user.id)
+  end
+
+  def contributor?(namespace)
+    namespace.team.contributors.exists?(current_user.id)
+  end
+
+  def viewer?(namespace)
+    namespace.team.viewers.exists?(current_user.id)
+  end
+
+  def can_pull?(namespace)
+    namespace.team.member_ids.include?(current_user.id)
   end
 end

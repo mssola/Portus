03401f1051d41eb5dbe2485a214ea0bdf70347d7
Miquel Sabaté Solà <msabate@suse.com>
2018-01-26 08:49:06 +0100

    asd
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

---
+++ config/webpack.js
@@ -0,0 +1,86 @@
+/* eslint-disable quote-props, comma-dangle */
+// This file is based on Gitlab's config/webpack.config.js file.
+
+const path = require('path');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+const StatsPlugin = require('stats-webpack-plugin');
+
+const ROOT_PATH = path.resolve(__dirname, '..');
+const IS_PRODUCTION = true
+
+var config = {
+  context: path.join(ROOT_PATH, 'assets'),
+
+  entry: {
+    application: './javascripts/main.js',
+    style: './stylesheets/main.scss',
+  },
+
+  output: {
+    path: path.join(ROOT_PATH, 'public/webpack'),
+    publicPath: '/assets/webpack/',
+    filename: IS_PRODUCTION ? '[name]-[chunkhash].js' : '[name].js',
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]',
+  },
+
+  plugins: [
+    // Manifest filename must match config.webpack.manifest_filename
+    // webpack-rails only needs assetsByChunkName to function properly
+    new StatsPlugin('manifest.json', {
+      chunkModules: false,
+      source: false,
+      chunks: false,
+      modules: false,
+      assets: true,
+    }),
+  ],
+
+  resolve: {
+    extensions: ['.js'],
+    mainFields: ['jsnext', 'main', 'browser'],
+    alias: {
+      '~': path.join(ROOT_PATH, 'app/assets/javascripts'),
+      'bootstrap/js': 'bootstrap-sass/assets/javascripts/bootstrap',
+    },
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    loaders: [].concat(
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|vendor\/assets)/,
+        loader: 'babel-loader',
+      },
+      {
+        test:/\.(s*)css$/,
+        exclude: /(node_modules|vendor\/assets)/,
+        use:['css-loader', 'sass-loader']
+      }
+    ),
+  },
+};
+
+if (IS_PRODUCTION) {
+  config.devtool = 'source-map';
+  config.plugins.push(
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') },
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+    }));
+}
+
+module.exports = config;
